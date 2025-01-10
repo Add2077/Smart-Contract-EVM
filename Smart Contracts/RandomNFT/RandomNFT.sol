6a008f30db67fb68cfbe58ce7632c1e2f9ba2acd
Add2077 <supachai.rarkthong@gmail.com>
2025-01-11 00:30:06 +0700

    Remove Chainlink VRF

--- Smart Contracts/RandomNFT/RandomNFT.sol
+++ Smart Contracts/RandomNFT/RandomNFT.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract RandomNFT is
@@ -20,77 +17,31 @@ contract RandomNFT is
     Ownable,
     AccessControl,
     Pausable,
-    VRFConsumerBaseV2,
     ReentrancyGuard
 {
-    enum MintType {
-        Normal,
-        Exchange
-    }
-
-    struct RequestStatus {
-        bool fulfilled;
-        address owner;
-        uint256[] exchangeTokenId;
-        uint256[] newTokenId;
-        MintType mintType;
-    }
     bytes32 public constant WORKER_ROLE = keccak256("WORKER_ROLE");
-
-    using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
     using Strings for uint256;
-
-    uint64 public s_subscriptionId;
-    bytes32 public s_keyHash;
-    uint32 public callbackGasLimit;
-    uint16 public requestConfirmations;
-    VRFCoordinatorV2Interface public COORDINATOR;
-    uint256 requestId;
+    uint256 private _numAvailableTokens;
 
     string private baseExtension = ".json";
     uint256 public maxSupply;
     string baseURI;
-    uint256 private _numAvailableTokens;
 
     mapping(uint256 => uint256) private _availableTokens;
-    mapping(uint256 => RequestStatus) requestMints;
+    event Minted(address owner, uint256 tokenId);
 
-    event MintNFTs(
-        address indexed to,
-        uint256 indexed tokenId,
-        uint256 requestId
-    );
-    event RequestMints(address indexed to, uint256 requestId);
-
-    constructor(
-        uint256 _maxSupply,
-        string memory _baseURIs,
-        address _vrfCoordinator,
-        uint64 _subscriptionId,
-        bytes32 _keyHash,
-        uint32 _callbackGasLimit,
-        uint16 _requestConfirmations
-    )
+    constructor(uint256 _maxSupply, string memory _baseURIs)
         Ownable(msg.sender)
-        VRFConsumerBaseV2(_vrfCoordinator)
-        ERC721("MoreMeme", "MOREMEME")
+        ERC721("Pokmon Trading Card Game", "Pokemon TCG")
     {
         maxSupply = _maxSupply;
         baseURI = _baseURIs;
         _numAvailableTokens = _maxSupply;
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
-        s_keyHash = _keyHash;
-        s_subscriptionId = _subscriptionId;
-        callbackGasLimit = _callbackGasLimit;
-        requestConfirmations = _requestConfirmations;
     }
 
     /**
      * @dev Revert receive and fallback functions.
      */
-
     receive() external payable {
         revert();
     }
@@ -99,82 +50,31 @@ contract RandomNFT is
         revert();
     }
 
-    // VRF set
-
-    function setVrfCoordinator(address _vrfCoordinator) external onlyOwner {
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
-    }
-
-    function setkeyHash(bytes32 _keyHash) external onlyOwner {
-        s_keyHash = _keyHash;
-    }
-
-    function setSubscriptionId(uint64 _subscriptionId) external onlyOwner {
-        s_subscriptionId = _subscriptionId;
-    }
-
-    function setCallbackGasLimit(uint32 _callbackGasLimit) external onlyOwner {
-        callbackGasLimit = _callbackGasLimit;
-    }
-
-    function setRequestConfirmations(uint16 _requestConfirmations)
-        external
-        onlyOwner
-    {
-        requestConfirmations = _requestConfirmations;
-    }
-
-    // VRF setBaseURIs
     function setBaseURIs(string memory _newBaseURI) external onlyOwner {
         baseURI = _newBaseURI;
     }
 
-    function totalMint() public view returns (uint256) {
-        return tokenIds.current();
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
 
-    function mintNFT() external  nonReentrant{
-        require(
-            tokenIds.current() <= maxSupply,
-            " Minting exceeds the maximum supply"
-        );
-        uint32 numWords = 1;
-        requestId = COORDINATOR.requestRandomWords(
-            s_keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
-        RequestStatus storage request = requestMints[requestId];
-        request.owner = msg.sender;
-        request.mintType = MintType.Normal;
-
-        emit RequestMints(msg.sender, requestId);
-    }
-
-    function fulfillRandomWords(
-        uint256 _requestId,
-        uint256[] memory _randomWords
-    ) internal override {
+    function randomMintNFT() external nonReentrant {
+        require(totalSupply() <= maxSupply, "Exceeds max supply");
         uint256 updatedNumAvailableTokens = _numAvailableTokens;
-        RequestStatus storage request = requestMints[_requestId];
-        address Owner = request.owner;
-        for (uint256 i = 0; i < _randomWords.length; i++) {
-            uint256 tokenId = getRandomAvailableTokenId(
-                updatedNumAvailableTokens,
-                _randomWords[i]
-            );
-            request.mintType == MintType.Normal;
-            emit MintNFTs(Owner, tokenId, _requestId);
+        uint256 randomSeed = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.number, msg.sender)
+            )
+        );
+        uint256 tokenId = getRandomAvailableTokenId(
+            updatedNumAvailableTokens,
+            randomSeed
+        );
+        _safeMint(msg.sender, tokenId);
+        --updatedNumAvailableTokens;
+        emit Minted(msg.sender, tokenId);
 
-            _safeMint(Owner, tokenId);
-            tokenIds.increment();
-            request.newTokenId.push(tokenId);
-            --updatedNumAvailableTokens;
-            request.fulfilled = true;
-            _numAvailableTokens = updatedNumAvailableTokens;
-        }
+        _numAvailableTokens = updatedNumAvailableTokens;
     }
 
     function getRandomAvailableTokenId(
@@ -182,8 +82,10 @@ contract RandomNFT is
         uint256 _randomWords
     ) internal returns (uint256) {
         uint256 randomNum = _randomWords;
-        uint256 randomIndex = (randomNum % updatedNumAvailableTokens) + 1;
-        return getAvailableTokenAtIndex(randomIndex, updatedNumAvailableTokens);
+        uint256 randomIndex = randomNum % updatedNumAvailableTokens;
+        return
+            getAvailableTokenAtIndex(randomIndex, updatedNumAvailableTokens) +
+            1;
     }
 
     function getAvailableTokenAtIndex(
@@ -193,33 +95,21 @@ contract RandomNFT is
         uint256 valAtIndex = _availableTokens[indexToUse];
         uint256 result;
         if (valAtIndex == 0) {
-            // This means the index itself is still an available token
             result = indexToUse;
         } else {
-            // This means the index itself is not an available token, but the val at that index is.
             result = valAtIndex;
         }
 
         uint256 lastIndex = updatedNumAvailableTokens - 1;
         if (indexToUse != lastIndex) {
-            // Replace the value at indexToUse, now that it's been used.
-            // Replace it with the data from the last index in the array, since we are going to decrease the array size afterwards.
             uint256 lastValInArray = _availableTokens[lastIndex];
             if (lastValInArray == 0) {
-                // This means the index itself is still an available token
                 _availableTokens[indexToUse] = lastIndex;
             } else {
-                // This means the index itself is not an available token, but the val at that index is.
                 _availableTokens[indexToUse] = lastValInArray;
-                // Gas refund courtsey of @dievardump
-                delete _availableTokens[lastIndex];
             }
         }
         return result;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
     }
 
     function tokenURI(uint256 _tokenId)
@@ -242,28 +132,15 @@ contract RandomNFT is
                 : "";
     }
 
-    function checkRequesStatus(uint256 _requestId)
-        public
-        view
-        returns (
-            bool fulfilled,
-            address owner,
-            uint256[] memory exchangeTokenId,
-            uint256[] memory newTokenId,
-            MintType mintType
-        )
+    function multiGrantRole(bytes32 _role, address[] memory _workerAddress)
+        external
+        onlyOwner
     {
-        RequestStatus memory request = requestMints[_requestId];
-        fulfilled = request.fulfilled;
-        owner = request.owner;
-        exchangeTokenId = request.exchangeTokenId;
-        newTokenId = request.newTokenId;
-        mintType = request.mintType;
+        uint256 _length = _workerAddress.length;
+        for (uint256 i = 0; i < _length; i++) {
+            grantRole(_role, _workerAddress[i]);
+        }
     }
-
-    /**
-     * @dev Override _increaseBalance.
-     */
 
     function _increaseBalance(address account, uint128 value)
         internal
@@ -272,9 +149,6 @@ contract RandomNFT is
         super._increaseBalance(account, value);
     }
 
-    /**
-     * @dev Override _update.
-     */
     function _update(
         address to,
         uint256 tokenId,
@@ -283,9 +157,6 @@ contract RandomNFT is
         return super._update(to, tokenId, auth);
     }
 
-    /**
-     * @dev require function for ERC721, ERC721Enumerable.
-     */
     function supportsInterface(bytes4 _interfaceId)
         public
         view
